c1d529b3832d9b260eaedd77e3e24d4a56ccb79c
jhice <jcguinez@gmail.com>
2024-03-03 23:59:29 +0100

    copy files + new favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addWatchTarget("src/css/");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
     dir: {
